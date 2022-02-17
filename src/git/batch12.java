ef0544dad31841d374de7a6ac657074f2b6f6214
vladajanca <vladajanca@gmail.com>
2022-02-16 21:39:49 -0500

    I have updated some code

--- src/git/batch12.java
+++ src/git/batch12.java
@@ -6,6 +6,8 @@ public class batch12 {
 		// TODO Auto-generated method stub
 
 		System.out.println("we we are were");
+		System.out.println("we we are were");
+		System.out.println("we we are were");
 	}
 
 }
